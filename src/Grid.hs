c1c271e168a27b3ba8346fc185af92a8d8f89b37
Apoorv Ingle <apoorv.ingle@gmail.com>
2016-11-29 19:52:50 -0600

    Can play with some bugs

--- src/Grid.hs
+++ src/Grid.hs
@@ -4,8 +4,9 @@ module Grid where
 
 import           Control.Applicative
 import           Data.List.Split
-import           Data.Map            (Map)
-import           Data.Text           hiding (chunksOf)
+import           Data.Map            (Map, fromList)
+import qualified Data.Map            as Map
+import qualified Data.Text           as T
 import           Debug.Trace
 import           Disc
 import           Graphics.Blank
@@ -75,12 +76,116 @@ adjacent (x, y) = Prelude.filter (\(a,b) -> a >= minX && a <= maxX
 
 direction :: Cord -> Cord -> Direction
 direction (nc_x, nc_y) (oc_x, oc_y)
-  | (nc_x > oc_x) && (nc_y > oc_y) = NE
+  | (nc_x > oc_x) && (nc_y > oc_y) = NW
   | (nc_x == oc_x) && (nc_y > oc_y) = N
-  | (nc_x < oc_x) && (nc_y > oc_y) = NW
-  | (nc_x < oc_x) && (nc_y == oc_y) = W
-  | (nc_x < oc_x) && (nc_y < oc_y) = SW
+  | (nc_x < oc_x) && (nc_y > oc_y) = NE
+  | (nc_x < oc_x) && (nc_y == oc_y) = E
+  | (nc_x < oc_x) && (nc_y < oc_y) = SE
   | (nc_x == oc_x) && (nc_y < oc_y) = S
-  | (nc_x > oc_x) && (nc_y < oc_y) = SE
-  | (nc_x > oc_x) && (nc_y == oc_y) = E
+  | (nc_x > oc_x) && (nc_y < oc_y) = SW
+  | (nc_x > oc_x) && (nc_y == oc_y) = W
 
+-- | Gives the next co-ordinate in the given direction
+moveInDirection :: Direction -> Cord -> Maybe Cord
+moveInDirection N (x,y)  = validate $ return (x, y-1)
+moveInDirection NE (x,y) = validate $ return (x+1,y-1)
+moveInDirection E (x,y)  = validate $ return (x+1,y)
+moveInDirection SE (x,y) = validate $ return (x+1,y+1)
+moveInDirection S (x,y)  = validate $ return (x,y+1)
+moveInDirection SW (x,y) = validate $ return (x-1,y+1)
+moveInDirection W (x,y)  = validate $ return (x-1,y)
+moveInDirection NW (x,y) = validate $ return (x-1,y-1)
+
+-- | It is a valid move if
+-- 1) The current pos is empty
+-- 2) There is an adjacent square with opposite colored disc
+-- 3) placing the disc creates a sandwich
+isValidMove :: Cord -> Map Cord Disc -> Disc -> Bool
+isValidMove pos board turn = isEmptySquare pos board
+  && areAdjacentSquareOpposite pos board turn
+  && sandwiches pos board turn
+
+-- | Condition 1) in @isValidMove@
+isEmptySquare :: Cord -> Map Cord Disc -> Bool
+isEmptySquare pos board = (Map.lookup pos board) == Nothing
+
+-- | Condition 2) in @isValidMove@
+areAdjacentSquareOpposite :: Cord -> Map Cord Disc -> Disc -> Bool
+areAdjacentSquareOpposite pos board turn = not . null
+  $ adjacentOppositeSquares pos board turn
+
+adjacentOppositeSquares :: Cord -> Map Cord Disc -> Disc -> [Maybe Disc]
+adjacentOppositeSquares  pos board turn =
+  filter (\e -> e /= Nothing && (e == (Just $ swap turn)))
+  $ fmap ((flip Map.lookup) board)
+  $ adjacent pos
+
+-- | condition 3) in @isValidMove@
+-- Select all adjacent squares that have opposite disc
+-- For each of those discs get first disk of same color in appropriate direction
+-- if any of such discs exist return True
+-- else return False
+sandwiches :: Cord -> Map Cord Disc -> Disc -> Bool
+sandwiches pos board turn = not . null $ filter (/=Nothing)
+  $  allFirstSameDiscs pos board turn
+
+allFirstSameDiscs pos board turn = sds <$> vps
+  where
+    ds = (toEnum <$> [0..7::Int])::[Direction]
+    l = \d -> moveInDirection d pos
+    ps = zip ds (l <$> ds)
+    vps = filter (\(a, Just b) -> (Map.lookup b board /= Just turn))
+          $ filter (\(a, b) -> (b /= Nothing))
+          $ (\(a, b) -> (a, validate b)) <$> ps
+    sds = \(d, (Just p)) -> getFirstSameDisc p d board turn
+
+-- | returns the co-ordinate of the first disc of the same color
+-- that appears after 1 or more opposite colored discs
+getFirstSameDisc :: Cord -> Direction -> Map Cord Disc -> Disc -> Maybe (Cord, Disc)
+getFirstSameDisc pos dir board turn = collapse $ head z
+  where
+    -- get the series of all the coordinates in the given direction
+    l = (Just pos)
+        : scanl (\c _ -> c >>= moveInDirection dir)
+                (Just pos >>= moveInDirection dir) l
+    md = ((flip Map.lookup) board =<<) <$> l
+    z =  dropWhile (\(a,b) -> (b == (Just $ swap turn)) && (b /= Nothing))
+      $ safeTail
+      $ zip l md
+
+collapse :: (Maybe a, Maybe b) -> Maybe (a,b)
+collapse ((Just x), (Just y)) = Just (x, y)
+collapse _                    = Nothing
+
+safeLast :: [a] -> Maybe a
+safeLast []     = Nothing
+safeLast (x:[]) = Just x
+safeLast (x:xs) = safeLast xs
+
+safeTail :: [a] -> [a]
+safeTail []     = []
+safeTail (x:xs) = xs
+
+safeHead :: [a] -> Maybe a
+safeHead []     = Nothing
+safeHead (x:xs) = Just x
+
+updateBoard :: Cord -> Disc -> Board -> Board
+updateBoard pos turn board = Map.union (fromList nv) board
+  where
+    z :: [(Direction, Maybe (Cord, Disc))]
+    z = zip ((toEnum <$> [0..7::Int])::[Direction]) $ allFirstSameDiscs pos board turn
+    bs = sequence $ concat $ between pos board <$> z
+    nv = case bs of
+      Just l  ->  zip l $ repeat turn
+      Nothing -> []
+
+between :: Cord -> Board -> (Direction, Maybe (Cord, Disc)) -> [Maybe Cord]
+between _ _ (_, Nothing)              = []
+between pos1 board (_, Just (pos2, disc)) =
+  takeWhile (\c -> c /= Just pos2 && c /= Nothing) l
+  where
+    l = (Just pos1)
+        : scanl (\c _ -> c >>= moveInDirection d)
+                (Just pos1 >>= moveInDirection d) l
+    d = direction pos1 pos2
--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Concurrent.STM.TVar
-import           Data.Map                    (Map, fromList)
+import           Data.Map                    (Map, fromList, toList)
 import qualified Data.Map                    as Map
 import           Debug.Trace
 import           Disc
@@ -32,7 +32,8 @@ viewer :: TVar Board -> DeviceContext ->IO ()
 viewer boardV context = do
   let (cw, ch, sz) = (width context, height context, min cw ch)
   board <- atomically $ readTVar boardV
-  print board
+  -- print board
+  print $ length $ Map.toList board
   send context $ do
     clearRect (0,0, cw, ch)
     beginPath()
@@ -61,6 +62,8 @@ viewer boardV context = do
 play :: TVar Board -> DeviceContext -> Disc -> IO ()
 play boardV context turn = do
   let (cw, ch, sz) = (width context, height context, min cw ch)
+  board <- atomically $ readTVar boardV
+  print board
   print $ "waiting for turn: " ++ show turn
   event <- wait context
   --print $ ePageXY event
@@ -72,7 +75,7 @@ play boardV context turn = do
       Just pos -> case Map.lookup pos board of
                     Nothing ->
                       if (isValidMove pos board turn)
-                      then do writeTVar boardV (Map.insert pos turn board)
+                      then do writeTVar boardV (updateBoard pos turn board)
                               return $ swap turn
                       else return turn
                       -- already something here
@@ -85,93 +88,6 @@ forever boardV context = do
         forkIO $ viewer boardV context
         play boardV context White
 
--- | It is a valid move if
--- 1) The current pos is empty
--- 2) There is an adjacent square with opposite colored disc
--- 3) placing the disc creates a sandwich
-isValidMove :: Cord -> Map Cord Disc -> Disc -> Bool
-isValidMove pos board turn = isEmptySquare pos board
-  && areAdjacentSquareOpposite pos board turn
-  && sandwiches pos board turn
-
--- | Condition 1) in @isValidMove@
-isEmptySquare :: Cord -> Map Cord Disc -> Bool
-isEmptySquare pos board = (Map.lookup pos board) == Nothing
-
--- | Condition 2) in @isValidMove@
-areAdjacentSquareOpposite :: Cord -> Map Cord Disc -> Disc -> Bool
-areAdjacentSquareOpposite pos board turn = not . null $ adjacentOppositeSquares pos board turn
-
-adjacentOppositeSquares :: Cord -> Map Cord Disc -> Disc -> [Maybe Disc]
-adjacentOppositeSquares  pos board turn =
-  filter (\e -> e /= Nothing && (e == (Just $ swap turn)))
-  $ fmap ((flip Map.lookup) board)
-  $ adjacent pos
-
--- | condition 3) in @isValidMove@
--- Select all adjacent squares that have opposite disc
--- For each of those discs get first disk of same color in appropriate direction
--- if any of such discs exist return True
--- else return False
-sandwiches :: Cord -> Map Cord Disc -> Disc -> Bool
-sandwiches pos board turn = not . null $ filter (/=Nothing) fsds
-  where
-    ds = (toEnum <$> [0..7::Int])::[Direction]
-    l = \d -> moveInDirection d pos
-    ps = zip ds (l <$> ds)
-    vps = filter (\(a, Just b) -> (Map.lookup b board /= Just turn))
-          $ filter (\(a, b) -> (b /= Nothing))
-          $ (\(a, b) -> (a, validate b)) <$> ps
-    sds = \(d, (Just p)) -> getFirstSameDisc p d board turn
-    fsds = sds <$> vps
-
-  -- adjacentOppositeSquares pos board turn
-
--- | returns the co-ordinate of the first disc of the same color
--- that appears after 1 or more opposite colored discs
-getFirstSameDisc :: Cord -> Direction -> Map Cord Disc -> Disc -> Maybe (Cord, Disc)
-getFirstSameDisc pos dir board turn = collapse $ head z
-  -- safeLast =<< (pure tail) <*> z
-  where
-    -- get the series of all the coordinates in the given direction
-    l = (Just pos)
-        : scanl (\c _ -> c >>= moveInDirection dir)
-                (Just pos >>= moveInDirection dir) l
-    md = ((flip Map.lookup) board =<<) <$> l
-    --mls = sequence $ takeWhile (/= Nothing) l
-    --mmds = sequence $ takeWhile (/= Just turn) md
-    z =  dropWhile (\(a,b) -> (b == (Just $ swap turn)))
-      $ safeTail
-      $ zip l md
-
-collapse :: (Maybe a, Maybe b) -> Maybe (a,b)
--- collapse = undefined
-collapse ((Just x), (Just y)) = Just (x, y)
-collapse _                    = Nothing
-
-safeLast :: [a] -> Maybe a
-safeLast []     = Nothing
-safeLast (x:[]) = Just x
-safeLast (x:xs) = safeLast xs
-
-safeTail :: [a] -> [a]
-safeTail []     = []
-safeTail (x:xs) = xs
-
-safeHead :: [a] -> Maybe a
-safeHead []     = Nothing
-safeHead (x:xs) = Just x
-
--- | Gives the next co-ordinate in the given direction
-moveInDirection :: Direction -> Cord -> Maybe Cord
-moveInDirection N (x,y)  = validate $ return (x, y-1)
-moveInDirection NE (x,y) = validate $ return (x+1,y-1)
-moveInDirection E (x,y)  = validate $ return (x+1,y)
-moveInDirection SE (x,y) = validate $ return (x+1,y+1)
-moveInDirection S (x,y)  = validate $ return (x,y+1)
-moveInDirection SW (x,y) = validate $ return (x-1,y+1)
-moveInDirection W (x,y)  = validate $ return (x-1,y)
-moveInDirection NW (x,y) = validate $ return (x-1,y-1)
 
 -- TODO comment this out after testing
 sampleBoard :: Map Cord Disc
@@ -179,3 +95,33 @@ sampleBoard = fromList [((-1,-1), Black),
                          ((-1,0), White),
                          ((0,0), Black),
                          ((0,-1), White)]
+
+sampleBoard' :: Map Cord Disc
+sampleBoard' = fromList [((-4,1),White),((-3,0),White),
+                         ((-3,1),White),((-2,-2),Black),
+                         ((-2,-1),White),((-2,0),Black),
+                         ((-1,-2),White),((-1,-1),Black),
+                         ((-1,0),Black),((0,-3),White),
+                         ((0,-2),Black),((0,-1),Black),
+                         ((0,0),Black)]
+
+sampleBoard'' :: Map Cord Disc
+sampleBoard'' = fromList [((-4,1),White),((-3,0),White),
+                          ((-3,1),White),((-2,-2),Black),
+                          ((-2,-1),White),((-2,0),Black),
+                          ((-1,-3),Black),((-1,-2),White),
+                          ((-1,-1),Black),((-1,0),Black),
+                          ((0,-4),Black),((0,-3),Black),
+                          ((0,-2),Black),((0,-1),Black),
+                          ((0,0),Black)]
+{-
+let {pos = (0,1); board = sampleBoard; turn = White}
+let {pos = (1,1); board = sampleBoard'; turn = White}
+-}
+
+-- TODO tests
+-- How do I generate valid board configurations?
+-- What are the properties of valid board configurations?
+-- 1) property after every move, total number of discs is increased only by one
+-- 2) increase in White == decrease in black + 1
+-- 3) increase in Black == decrease in white + 1
